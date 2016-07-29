80ec6858c84bebc0201b2f182635c4f6ceff837e
Todd Croak-Falen <Todd Croak-Falen>
2016-07-29 15:58:48 -0700

    Began refining HTML and CSS. Began pseudo coding JavaScript.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
 
+// Declare all necessary variables outside of the game.
+// List all the questions.
 
+// Call game function.
 
-
+// Push Q1 question to #question span.
+// Push Q1 answers to #answers span.
 
 };
