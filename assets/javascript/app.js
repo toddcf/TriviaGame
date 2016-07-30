d92732f3bf21b9bc7bbc3df6cfba0130c72e6e1f
Todd Croak-Falen <Todd Croak-Falen>
2016-07-30 12:27:34 -0700

    Added more JavaScript pseudo code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,39 @@
 $( document ).ready(function() {
 
 // Declare all necessary variables outside of the game.
-// List all the questions.
+// Array of questions.
+// Array of answers for each question.
 
-// Call game function.
+// On Click of "Start" button, call game function.
 
-// Push Q1 question to #question span.
-// Push Q1 answers to #answers span.
+	// Declare all necessary variables inside of the game.
+		// timer
 
-// If Q1 is correct...
-// If Q1 is incorrect...
+	// Uuse for loop(?) to push first question from array to #question span.
+		// Reset timer to start point.
+			// (45 seconds? Adjust when testing game to find best amount of time.)
+		// Begin counting down.
 
-// Advance to the next question.
+	// Use for loop(?) to push first "answers" array to #answers span.
+		// Array randomly chooses order in which to display the answers each time.
+
+	// If Q1 is correct...
+		// Display image.
+		// Congratulate player.
+		// Add to "correct" tally.
+		// empty div or span.
+	// If Q1 is incorrect...
+		// Display image.
+		// Tell player this was incorrect. Notify of correct answer.
+		// Add to "incorrect" tally.
+		// empty div or span.
+
+	// If timer runs out, question is marked incorrect.
+		// Copy and paste from "If Q1 is incorrect" above.
+
+	// Advance to the next question.
+
+	// After last question, display score, etc., and "Play Again?" button.
+	// On Click of "Play Again?", reset the game.
 
 };
